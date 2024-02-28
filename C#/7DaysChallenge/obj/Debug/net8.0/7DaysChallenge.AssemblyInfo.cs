85e69565da18b182daf7ecbaba7eb645a54b1421
Rena7oSouza <renatofrancisco@hotmail.com>
2024-02-28 12:57:48 -0300

    showPokemon0.3

--- C#/7DaysChallenge/obj/Debug/net8.0/7DaysChallenge.AssemblyInfo.cs
+++ C#/7DaysChallenge/obj/Debug/net8.0/7DaysChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("7DaysChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c68b908276053a11813786723b4925df2bb56e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aaa7a2628d71cdcf3779fc0ab40f67cafa5c70d")]
 [assembly: System.Reflection.AssemblyProductAttribute("7DaysChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("7DaysChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

baa843b6ea7c0ac8bda5dcb29d8005859dcc7856
Renato Souza <renatofrancisco@hotmail.com>
2024-04-09 06:58:08 -0300

    menu fixed

--- Javascript/StaticPage/script.js
+++ Javascript/StaticPage/script.js
@@ -1,38 +1,46 @@
 const open_btn = document.querySelector('.open-btn')
 const close_btn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
+const mainContent = document.getElementById('main-content');
 
 open_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.add('visible'))
-})
+    nav.forEach(nav_el => nav_el.classList.add('visible'));
+    mainContent.classList.add('menu-open');
+});
 
 close_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.remove('visible'))
-})
+    nav.forEach(nav_el => nav_el.classList.remove('visible'));
+    mainContent.classList.remove('menu-open');
+});
 
-const textElement = document.getElementById('text');
-const textContent = "Mobile Navigation";
-const cursor = document.createElement('span');
-cursor.classList.add('cursor');
+
+
+let titles = ['Hello, I\'m Renato Souza', 'Welcome to my portfolio!'];
+let i = 0;
+let titleIndex = 0;
 
 function typeWriter() {
-    let i = 0;
-    const typing = setInterval(() => {
-        if (i < textContent.length) {
-            textElement.innerHTML += textContent[i];
-            i++;
-        } else {
-            clearInterval(typing);
-            textElement.parentNode.insertBefore(cursor, textElement.nextSibling); // Adicionando o cursor após o texto
-            cursor.style.visibility = 'visible'; // Mostra o cursor
-            setTimeout(() => {
-                textElement.innerHTML = ''; // Limpa o texto
-                cursor.style.visibility = 'hidden'; // Esconde o cursor
-                i = 0;
-                typeWriter();
-            }, 5000); // Digita novamente após 5 segundos
-        }
-    }, 100);
+    if (i < titles[titleIndex].length) {
+        document.getElementById('text').innerHTML += titles[titleIndex].charAt(i);
+        i++;
+        setTimeout(typeWriter, 100);
+    } else {
+        document.getElementById('text').style.animation = 'none';
+        let delayBeforeNextTitle = titles[titleIndex] === 'Hello, I\'m Renato Souza' ? 0 : 2000;
+        setTimeout(() => {
+            document.getElementById('text').innerHTML = '';
+            document.getElementById('text').style.animation = '';
+            i = 0;
+            titleIndex = (titleIndex + 1) % titles.length;
+            let newTitle = document.getElementById('text').cloneNode(true);
+            document.getElementById('text').parentNode.replaceChild(newTitle, document.getElementById('text'));
+            setTimeout(typeWriter, delayBeforeNextTitle);
+        }, 3000);
+    }
 }
 
-typeWriter();
+setTimeout(typeWriter, 4000);
+
+
+
+

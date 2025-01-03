f7806031ac510fd10599a766518bbcbdc1765fb5
Renato Souza <127121558+Rena7oSouza@users.noreply.github.com>
2025-01-03 07:23:03 -0300

    Delete Django/finalproject directory

--- Django/finalproject/training/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, WorkoutDetail, Exercise, MuscleGroup
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(WorkoutDetail)
-admin.site.register(Exercise)
-admin.site.register(MuscleGroup)
